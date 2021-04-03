c45da867760f3d6c01664d149a3c9840c16167fe
KozhevnikovaJulia <Kozhevnikova1501@yandex.ru>
2021-04-03 21:37:46 +0300

    fix

--- sendMessageRouter.js
+++ sendMessageRouter.js
@@ -2,8 +2,8 @@ const nodemailer = require('nodemailer');
 const express = require('express');
 const { query } = require('express');
 const router = express.Router();
-const login = process.env.LOGIN || '';
-const password = process.env.PASSWORD || '';
+const login = process.env.LOGIN || '---';
+const password = process.env.PASSWORD || '----';
 
 router.use(function timeLog(req, res, next) {
   console.log('Time: ', Date.now());
